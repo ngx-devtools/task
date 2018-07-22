bf55be7f61ff6798291d2b9cea9f9b882928057b
aelbore <aelbore.sg@gmail.com>
2018-07-22 11:22:54 +0800

    add args for the builds, update beta version

--- src/build.ts
+++ src/build.ts
@@ -6,30 +6,35 @@ const prodModeParams = [ '--prod',  '--prod=true',  '--prod true'  ];
 
 const argv = require('yargs')
   .option('elements', { default: null, type: 'string' })
+  .option('prod-elements', { default: null, type: 'string' })
+  .option('prod-libs', { default: null, type: 'string' })
+  .option('prod-app', { default: null, type: 'string' })
   .argv;
 
 const ELEMENTS_PATH = join('src', 'elements');
 
-async function buildDevElementsArgv(){
-  return (argv.elements) 
-    ? buildDevElements({ src: ELEMENTS_PATH, packages: argv.elements.split('.')  })
-    : buildDevElements({ src: ELEMENTS_PATH });
-}
-
-async function buildDevTask(){
-  return (argv.elements !== null) ? buildDevElementsArgv(): buildDevAll()
+async function buildTask(){
+  return (argv.prodElements !== null) 
+    ? (argv.prodElements)
+      ? buildProdElements({ src: ELEMENTS_PATH, packages: argv.prodElements.split('.')  })
+      : buildProdElements({ src: ELEMENTS_PATH })
+    : (argv.elements !== null)
+      ? (argv.elements) 
+        ? buildDevElements({ src: ELEMENTS_PATH, packages: argv.elements.split('.')  })
+        : buildDevElements({ src: ELEMENTS_PATH })
+    : buildDevAll()
 }
 
 async function buildProdTask(){
-  return (argv.elements !== null) 
-    ? (argv.elements)
+  return (argv.elements !== null)
+    ? (argv.elements) 
       ? buildProdElements({ src: ELEMENTS_PATH, packages: argv.elements.split('.')  })
       : buildProdElements({ src: ELEMENTS_PATH })
     : Promise.resolve()
 }
 
 async function build() {
-  return (isProcess(prodModeParams)) ? buildProdTask(): buildDevTask();
+  return (isProcess(prodModeParams)) ? buildProdTask(): buildTask();
 }
 
 export { build }
