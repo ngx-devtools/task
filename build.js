937d71b29181dd48d8328831ad8d4b9891ab2651
aelbore <aelbore@gmail.com>
2018-10-05 09:30:13 +0800

    [refactor] add rollup.config

--- build.js
+++ build.js
@@ -1,23 +1,5 @@
 
-const { createRollupConfig, ngxBuild } = require('@ngx-devtools/common');
+const { ngxBuild } = require('@ngx-devtools/common');
+const { getRollupConfig } = require('./rollup.config');
 
-(async function(){
-  const PKG_NAME = 'task';
-  const ENTRY_FILE = `.tmp/${PKG_NAME}.ts`;
-
-  const rollupConfig = createRollupConfig({
-    input: ENTRY_FILE,
-    tsconfig: '.tmp/tsconfig.json',
-    external: [ 
-      '@ngx-devtools/common',
-      '@ngx-devtools/build',
-      '@ngx-devtools/server'
-    ],
-    output: {
-      file: `dist/${PKG_NAME}.js`,
-      format: 'cjs'
-    }
-  })
-
-  await ngxBuild(PKG_NAME, rollupConfig)
-})()
+ngxBuild('task', getRollupConfig());
